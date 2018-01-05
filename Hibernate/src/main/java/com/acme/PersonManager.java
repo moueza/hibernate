7373ad1dee3a17baae813128c3ed0bb7cdb78a14
Peter MOUEZA <mouezapeter@gmail.com>
2018-01-05 12:58:06 +0100

    Struts structure

---
+++ Hibernate/src/main/java/com/acme/PersonManager.java
@@ -0,0 +1,17 @@
+package com.acme;
+
+/**
+ * Renaming Acegi Security to Spring Security
+ * https://spring.io/blog/2007/01/25/why-the-name-acegi TODO bean Entity
+ */
+public class PersonManager {
+	Integer cpt;
+
+	public Integer getCpt() {
+		return cpt;
+	}
+
+	public void setCpt(Integer cpt) {
+		this.cpt = cpt;
+	}
+}
